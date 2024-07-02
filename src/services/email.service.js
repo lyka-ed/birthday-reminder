8b0b51842384ec33c31fa062f8a205127025ef6c
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-07-02 10:28:51 +0100

    fix: email template

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const sendEmail = async (recipient, name) => {
@@ -15,93 +17,19 @@ const sendEmail = async (recipient, name) => {
     },
   });
 
+  const username = name.charAt(0).toUpperCase() + name.slice(1);
+
+  const emailtemplatePath = path.join(__dirname, "../utils/emailTemplate.html");
+  let emailtemplate = fs.readFileSync(emailtemplatePath, "utf-8");
+
+  // Replace placeholders with actual values
+  emailtemplate = emailtemplate.replace("${username}", username);
+
   const mailOptions = {
     from: process.env.MAIL_USERNAME,
     to: recipient,
-    subject: "Happy Birthday!",
-    html: `<!DOCTYPE html>
-            <html lang="en">
-              <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Birthday Greeting</title>
-                <style>
-                  * {
-                    box-sizing: border-box;
-                    margin: 0;
-                    padding: 0;
-                  }
-                  body {
-                    font-family: Arial, sans-serif;
-                    font-size: 16px;
-                    background-color: #f4f4f4;
-                  }
-
-                  .main__container {
-                    max-width: 600px;
-                    margin: 20px auto;
-                    padding: 20px;
-                    width: 100%;
-                    border-radius: 8px;
-                  }
-
-                  .heading {
-                    padding: 25px 0;
-                    background-color: #007bff;
-                    text-align: center;
-                    color: #ffffff;
-                    font-size: 28px;
-                    font-weight: bold;
-                    border-radius: 8px 8px 0 0;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-                  }
-
-                  .message__container {
-                    padding: 20px;
-                    background-color: #ffffff;
-                    color: #333333;
-                    font-size: 1.05rem;
-                    line-height: 1.5;
-                    text-align: left;
-                  }
-
-                  .message__container p {
-                    margin-bottom: 10px;
-                  }
-
-                  .footer__container {
-                    padding: 20px;
-                    font-size: 14px;
-                    color: #777777;
-                    text-align: center;
-                  }
-                </style>
-              </head>
-              <body>
-                <div class="main__container">
-                  <div class="heading">Happy Birthday!!! 🎉</div>
-
-                  <div class="message__container">
-                    <p>Dear ${username},</p>
-                    <p>
-                      Wishing you a day filled with happiness and a year filled with joy.
-                      Happy birthday!
-                    </p>
-                    <p>
-                      May your special day be packed with all the joy and blessings you can
-                      imagine. Enjoy every moment to the fullest!
-                    </p>
-                    <p>Best wishes,</p>
-                    <p style="margin: 0"><b>Josh</b>,<br />Jubilix</p>
-                  </div>
-
-                  <div class="footer__container">
-                    <p>&copy; 2024 Lyka. All rights reserved.</p>
-                  </div>
-                </div>
-              </body>
-            </html>
-  `,
+    subject: "Happy Birthday !!",
+    html: emailtemplate,
   };
 
   try {
