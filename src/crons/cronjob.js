5e45d275a791d999508d9e677478ad806e65fda6
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-07-02 10:25:16 +0100

    fix: cronjob time

--- src/crons/cronjob.js
+++ src/crons/cronjob.js
@@ -2,11 +2,12 @@ const cron = require("node-cron");
 const sendEmail = require("../services/email.service");
 const userService = require("../services/user.service");
 
+// To check, retrieve and send birthday mails.
 const checkBirthdays = async () => {
-  console.log("Check your birthday.");
+  console.log("Checking birthdays......");
   const today = new Date();
-  const day = today.getDay();
-  const month = today.getMonth() + 1;
+  const currentDay = today.getDay();
+  const currentMonth = today.getMonth() + 1;
 
   try {
     const users = await userService.getAllUsers();
@@ -16,22 +17,24 @@ const checkBirthdays = async () => {
       const birthDay = user.birthdate.getDay();
       const birthMonth = user.birthdate.getMonth() + 1;
 
-      if (birthDay === day && birthMonth === month) {
-        await sendEmail(user.email, user.username);
-      }
+      // Check if user's birthday is today
+      // if (birthDay === currentDay && birthMonth === currentMonth) {
+      await sendEmail(user.email, user.username);
+      // }
     });
   } catch (error) {
-    console.error(error);
+    console.error("Error checking for birthday", error);
   } finally {
     console.log("Cronjob ended");
   }
 };
 
+// Cronjob will start at 7am daily to check for user an send email on the date of their birthday.
 const startCronJob = () => {
   cron.schedule("* * * * *", checkBirthdays, {
     timezone: "Africa/Lagos",
   });
-  console.log("Cronjob is active");
+  console.log("Birthday reminder cronjob  is active");
 };
 
 module.exports = startCronJob;
